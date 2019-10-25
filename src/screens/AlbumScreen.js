c1bfbbc80705495e5cf508feb3bc38e885be63f5
Matheus Castro <matheuscastro@gmail.com>
2019-10-25 13:09:56 -0300

    Primeira tela

---
+++ src/screens/AlbumScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const AlbumScreen = () => {
+  return <Text>Teste</Text>;
+};
+
+AlbumScreen.navigationOptions = {
+  title: 'Álbums',
+};
+
+const styles = StyleSheet.create({});
+
+export default AlbumScreen;
